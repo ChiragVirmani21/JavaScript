f7658e836d250ca646c1d95d255c38da41056bf4
Chirag Virmani <121285647+ChiragVirmani21@users.noreply.github.com>
2023-01-03 06:49:06 +0000

    datattypeConversion

---
+++ Basics/conversionOperations.js
@@ -0,0 +1,29 @@
+// let score = null
+// console.log(typeof(score));// we can use type of in 2 ways (1)
+// console.log(typeof score); // (2)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"      => 33
+//"33abc"    => NaN(Not a Number)
+// true      => 1; false => 0
+//"chirag"   => NaN
+// null      => 0
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1       => true; 0 => false
+// ""      => false
+//"chirag" => true
+
+let someNumber = 123
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
